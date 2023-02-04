45d45a2b9285d5443cdc0c7b6360757ce706f22b
ikecchi666 <molmolituki@gmail.com>
2023-02-04 16:22:41 +0900

    ドキュメントの整理

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
@@ -13,6 +14,11 @@ body {
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>総人口推移</title>
+        <meta name="description" content="都道府県別の総人口推移" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
